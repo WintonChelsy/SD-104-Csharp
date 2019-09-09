93b06c36ee0cbba40ab739f04501c324588c9374
WintonChelsy <54279565+WintonChelsy@users.noreply.github.com>
2019-09-09 10:55:02 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My cat is a trash goblin...");
+            Console.ReadLine();
+        }
+    }
+}
